bc1a5454334eb7d708ee63bed291c900f4f115df
AlexBonilla57 <bonilla.alex57@gmail.com>
2017-03-13 15:57:48 -0700

    Add files via upload

---
+++ UnityAdsExample.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+#if UNITY_ADS
+using UnityEngine.Advertisements;
+#endif
+
+public class UnityAdsExample : MonoBehaviour
+{
+    void Start()
+    {
+        Advertisement.Initialize("1344541", true);
+
+        StartCoroutine(ShowAdWhenReady());
+    }
+
+    System.Collections.IEnumerator ShowAdWhenReady()
+    {
+        while (!Advertisement.IsReady())
+            yield return null;
+
+        Advertisement.Show();
+    }
+
+
+}
